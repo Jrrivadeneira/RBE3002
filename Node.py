b0860b14a198d652b44b42b6a550f76e71954676
jack rivadeneira <jrrivadeneira@wpi.edu>
2015-04-21 15:42:44 -0400

    Updated Node.py and hopefully this whole thing works now

--- Node.py
+++ Node.py
@@ -4,4 +4,16 @@ class Node:
 	fscore = 0
 	parent = 0
 	cost = 0
-	
+
+	"""Returns the FScore of itself"""
+	def FScore(self):
+		return fscore
+	"""sets the current parent to the given parent"""
+	def setParent(self,newParent):
+		self.parent = newParent
+	"""sets the current fscore to the given fscore"""
+	def setFScore(self,newFScore):
+		self.fscore = newFScore
+	"""Its a beautiful day."""
+	def getRogers(self):
+		return self.rogers
